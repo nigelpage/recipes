7f63ff88da3d8d251f0fcae02949e2eef46bb114
Nigel Page <nigelp@synteca.com>
2023-08-05 16:58:55 +1000

    Add ingredient

--- ingredients/domain/ingredient.go
+++ ingredients/domain/ingredient.go
@@ -2,12 +2,13 @@ package ingredients
 
 import (
 	"github.com/ajstarks/svgo"
+	"github.com/google/uuid"
 )
 
 type ingredient struct {
-	id int64;
+	id uuid.UUID;
 	name string;
 	image svg.SVG;
 	description string;
-	alternates []int64
+	alternates []uuid.UUID
 }
