3f289b7ec6ff601b8fe0667098c82be7cee8ec91
Nigel Page <nigelp@synteca.com>
2023-08-06 13:11:00 +1000

    Create basic Ingredient domain

--- ingredients/domain/ingredient.go
+++ ingredients/domain/ingredient.go
@@ -5,10 +5,22 @@ import (
 	"github.com/google/uuid"
 )
 
-type ingredient struct {
+type Ingredient struct {
 	id uuid.UUID;
-	name string;
-	image svg.SVG;
-	description string;
+	Name string;
+	Image svg.SVG;
+	Description string;
 	alternates []uuid.UUID
+}
+
+type AccessIngredient interface {
+	FindByName(name string) (ingredients []Ingredient, err error)
+	Delete() (err error)
+	Update() (err error)
+	GetAlternateIngredients() (ingredients []Ingredient, err error)
+}
+
+func NewIngredient(name string, description string) (ingredient Ingredient) {
+	ing := Ingredient{id: uuid.New(), Name: name, Description: description, alternates: []uuid.UUID{}}
+	return ing
 }
